81b8b6385137ba007b1302f498844d246bfeae2c
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-29 18:16:24 -0400

    adds demo gif

--- example.js
+++ example.js
@@ -5,13 +5,10 @@ enquirer.register('checkbox', require('./'));
 enquirer.question('color', 'What is your favorite color?', {
   default: 'blue',
   type: 'checkbox',
-  // choices: ['red', 'yellow', 'blue']
+  choices: ['red', 'yellow', 'blue']
 });
 
 enquirer.ask('color')
   .then(function(answers) {
     console.log(answers)
   })
-  .catch(function(err) {
-    console.log(err)
-  });
