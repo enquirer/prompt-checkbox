f893cbf6ec6ec8660a83c4185edb77ab1af72b2d
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-30 07:00:28 -0400

    improve logic for `setDefault`
    - remove unused code
    - fix selected item styling
    - make `.getAnswerValue` loop once
    - use prompt-base

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var util = require('util');
 var Paginator = require('terminal-paginator');
-var Prompt = require('enquirer-prompt');
+var Prompt = require('prompt-base');
 var radio = require('radio-symbol');
 var cursor = require('cli-cursor');
 var log = require('log-utils');
@@ -16,8 +16,7 @@ function Checkbox(/*question, answers, rl*/) {
   if (!this.question.choices) {
     throw new Error('expected "options.choices" to be an array');
   }
-  this.setDefault();
-  this.pointer = 0;
+  this.pointer = ~this.setDefault();
   this.question.default = null;
   this.paginator = new Paginator();
 }
@@ -68,8 +67,8 @@ Checkbox.prototype.render = function(error) {
   if (this.status === 'answered') {
     message += log.cyan(this.selection.join(', '));
   } else {
-    var choicesStr = renderChoices(this.question.choices, this.pointer);
-    var idx = this.question.choices.indexOf(this.question.choices.getChoice(this.pointer));
+    var choicesStr = renderChoices(this.choices, this.pointer, this.options);
+    var idx = this.choices.indexOf(this.choices.getChoice(this.pointer));
     message += '\n' + this.paginator.paginate(choicesStr, idx, this.question.pageSize);
   }
 
@@ -92,38 +91,54 @@ Checkbox.prototype.onError = function(answer) {
  */
 
 Checkbox.prototype.onSubmit = function(line) {
-  this.answer = this.getCurrentValue(line);
+  this.answer = this.getAnswerValue(line);
   this.status = 'answered';
-  this.render();
-  this.ui.write();
-  cursor.show();
-  this.callback(this.answer);
+  this.on('answer', function() {
+    cursor.show();
+  });
+  this.submitAnswer();
 };
 
+/**
+ * When user presses the `up` key
+ */
+
 Checkbox.prototype.onUpKey = function() {
-  var len = this.question.choices.realLength;
+  var len = this.choices.realLength;
   this.pointer = (this.pointer > 0) ? this.pointer - 1 : len - 1;
   this.render();
 };
 
+/**
+ * When user presses the `down` key
+ */
+
 Checkbox.prototype.onDownKey = function() {
-  var len = this.question.choices.realLength;
+  var len = this.choices.realLength;
   this.pointer = (this.pointer < len - 1) ? this.pointer + 1 : 0;
   this.render();
 };
 
+/**
+ * When user presses a number key
+ */
+
 Checkbox.prototype.onNumberKey = function(event) {
   var num = Number(event.value);
-  if (num <= this.question.choices.realLength) {
+  if (num <= this.choices.realLength) {
     this.pointer = num - 1;
-    this.toggleChoice(this.pointer);
+    this.question.choices.toggleChoice(this.pointer);
   }
   this.render();
 };
 
+/**
+ * When user presses the `space` bar
+ */
+
 Checkbox.prototype.onSpaceKey = function() {
   this.spaceKeyPressed = true;
-  this.toggleChoice(this.pointer);
+  this.choices.toggleChoice(this.pointer);
   this.render();
 };
 
@@ -132,41 +147,33 @@ Checkbox.prototype.onSpaceKey = function() {
  */
 
 Checkbox.prototype.setDefault = function() {
-  if (Array.isArray(this.question.default)) {
-    var len = this.question.choices.length;
-    var idx = -1;
-    while (++idx < len) {
-      var choice = this.question.choices[idx];
-      if (contains(this.question.default, choice.value)) {
-        choice.checked = true;
-      }
-    }
+  if (this.question.hasDefault) {
+    var idx = this.question.choices.getIndex(this.question.default);
+    this.question.choices.enable(idx);
+    return idx;
   }
+  return -1;
 };
 
 /**
  * Get the currently selected value
  */
 
-Checkbox.prototype.getCurrentValue = function() {
-  var choices = this.question.choices.filter(function(choice) {
-    return Boolean(choice.checked) && !choice.disabled;
-  });
-  this.selection = choices.map(function(choice) {
-    return choice.short;
-  });
-  return choices.map(function(choice) {
-    return choice.value;
-  });
-};
+Checkbox.prototype.getAnswerValue = function() {
+  this.selection = [];
+  var choices = this.question.choices.realChoices;
+  var len = choices.length;
+  var idx = -1;
+  var res = [];
 
-/**
- * Toggle the choice at the given `index`
- */
-
-Checkbox.prototype.toggleChoice = function(idx) {
-  var checked = this.question.choices.getChoice(idx).checked;
-  this.question.choices.getChoice(idx).checked = !checked;
+  while (++idx < len) {
+    var choice = choices[idx];
+    if (choice.checked && !choice.disabled) {
+      this.selection.push(choice.alias || choice.short);
+      res.push(choice.value);
+    }
+  }
+  return res;
 };
 
 /**
@@ -175,29 +182,39 @@ Checkbox.prototype.toggleChoice = function(idx) {
  * @return {String} Rendered choices string
  */
 
-function renderChoices(choices, pointer) {
+function renderChoices(choices, pointer, options) {
+  var offset = 0;
   var output = '';
-  var separatorOffset = 0;
 
   choices.forEach(function(choice, i) {
     if (choice.type === 'separator') {
-      separatorOffset++;
-      output += ' ' + choice;
+      offset++;
+      output += inactive(choice);
 
     } else if (choice.disabled) {
-      separatorOffset++;
+      offset++;
       output += disabled(choice);
 
-    } else {
-      var isSelected = (i - separatorOffset === pointer);
-      output += isSelected ? log.cyan('❯') : ' ';
-      output += getCheckbox(choice) + ' ' + choice.name;
-    }
+    } else if (i - offset === pointer) {
+      output += active(choice, renderChoice(choice), options);
 
-    output += '\n';
+    } else {
+      output += inactive(choice, renderChoice(choice));
+    }
+    output += ' \n';
   });
 
   return output.replace(/\n$/, '');
+}
+
+/**
+ * Render the checkbox based on state.
+ * @param  {Boolean} `checked` If enabled, adds an X to the checkbox
+ * @return {String} returns the rendered radio-button string
+ */
+
+function renderChoice(choice) {
+  return choice.checked ? log.green(radio.on) : radio.off;
 }
 
 /**
@@ -209,53 +226,31 @@ function disabled(choice) {
   return log.dim(symbol + choice.name + ' (' + (choice.disabled || 'Disabled') + ')');
 }
 
-function inactive(choice) {
-  return '  ' + choice.name;
+function inactive(choice, prefix) {
+  return ' ' + (prefix || '') + ' ' + choice.name;
 }
 
-function active(choice, options) {
-  return log.cyan(pointer(options) + choice.name);
+function active(choice, prefix, options) {
+  if (prefix && typeof prefix === 'object') {
+    options = prefix;
+    prefix = null;
+  }
+  return log.cyan(pointer(options)) + (prefix || '') + ' ' + choice.name;
 }
 
 function pointer(options) {
-  if (typeof options.pointer === 'string') {
+  if (options && typeof options.pointer === 'string') {
     return options.pointer.trim() + ' ';
   }
   switch(process.platform) {
     case 'win32':
-      return '> ';
+      return '>';
     case 'linux':
-      return '‣ ';
+      return '‣';
     default: {
-      return '❯ ';
+      return '❯';
     }
   }
-}
-
-/**
- * Get the checkbox based on state.
- * @param  {Boolean} `checked` If active/checked, adds an X to the checkbox
- * @return {String} Checkbox string
- */
-
-function getCheckbox(choice) {
-  return choice.checked ? log.green(radio.on) : radio.off;
-}
-
-/**
- * Cast `val` to an array.
- */
-
-function arrayify(val) {
-  return val ? (Array.isArray(val) ? val : [val]) : [];
-}
-
-/**
- * Return true if `arr` contains the given `element`
- */
-
-function contains(val, ele) {
-  return arrayify(val).indexOf(ele) !== -1;
 }
 
 /**
