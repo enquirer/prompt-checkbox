e75f3d91ee3bffdeb38bde07a2ee707c4830b291
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-21 06:42:53 -0400

    refactor based on changes in prompt-base

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var util = require('util');
-var Paginator = require('terminal-paginator');
+var debug = require('debug')('prompt-checkbox');
 var Prompt = require('prompt-base');
-var radio = require('radio-symbol');
 var cursor = require('cli-cursor');
 var log = require('log-utils');
 
@@ -11,22 +9,23 @@ var log = require('log-utils');
  * Checkbox prompt
  */
 
-function Checkbox(/*question, answers, rl*/) {
+function Checkbox(/*question, answers, ui*/) {
+  debug('initializing from <%s>', __filename);
   Prompt.apply(this, arguments);
-  if (!this.question.choices) {
-    throw new Error('expected "options.choices" to be an array');
+  if (!this.choices) {
+    throw new TypeError('expected "options.choices" to be an object or array');
   }
+  this.checked = [];
+  this.position = 0;
   this.setDefault();
-  this.pointer = 0;
   this.question.default = null;
-  this.paginator = new Paginator();
 }
 
 /**
  * Inherit Prompt
  */
 
-util.inherits(Checkbox, Prompt);
+Prompt.extend(Checkbox);
 
 /**
  * Start the prompt session
@@ -37,15 +36,17 @@ util.inherits(Checkbox, Prompt);
 Checkbox.prototype.ask = function(cb) {
   this.callback = cb;
 
-  this.ui.on('up', this.onUpKey.bind(this));
-  this.ui.on('down', this.onDownKey.bind(this));
-  this.ui.on('space', this.onSpaceKey.bind(this));
-  this.ui.on('number', this.onNumberKey.bind(this));
-
-  this.ui.once('line', this.onSubmit.bind(this));
   this.ui.once('error', this.onError.bind(this));
+  this.ui.on('line', this.onSubmit.bind(this));
+  this.ui.on('keypress', function(event) {
+    this.move(event.key.name, event);
+    if (event.key.name === 'space') {
+      this.spaceKeyPressed = true;
+      this.render();
+    }
+  }.bind(this));
 
-  // Init the prompt
+  // Initialize prompt
   cursor.hide();
   this.render();
   return this;
@@ -55,44 +56,33 @@ Checkbox.prototype.ask = function(cb) {
  * Render the prompt to the terminal
  */
 
-Checkbox.prototype.render = function(error) {
-  // Render question
-  var message = this.message;
-  var append = '';
+Checkbox.prototype.render = function(state) {
+  var append = typeof state === 'string'
+    ? log.red('>> ') + state
+    : '';
 
+  // render question
+  var message = this.message;
   if (!this.spaceKeyPressed) {
     message += '(Press <space> to select) ';
   }
 
   // Render choices or answer depending on the state
   if (this.status === 'answered') {
-    message += log.cyan(this.selection.join(', '));
+    message += log.cyan(this.checked.join(', '));
   } else {
-    var choicesStr = renderChoices(this.choices, this.pointer, this.options);
-    var idx = this.choices.indexOf(this.choices.getChoice(this.pointer));
-    message += '\n' + this.paginator.paginate(choicesStr, idx, this.question.pageSize);
+    message += this.choices.render(this.position, {paginate: true});
   }
 
-  if (error) {
-    append = log.red('>> ') + error;
-  }
   this.ui.render(message, append);
-};
-
-/**
- * When an error event is emitted
- */
-
-Checkbox.prototype.onError = function(answer) {
-  this.render(answer.isValid);
 };
 
 /**
  * When user press `enter` key
  */
 
-Checkbox.prototype.onSubmit = function(line) {
-  this.answer = this.getAnswerValue(line);
+Checkbox.prototype.onSubmit = function() {
+  this.answer = this.getChecked();
   this.status = 'answered';
   this.on('answer', function() {
     cursor.show();
@@ -101,56 +91,12 @@ Checkbox.prototype.onSubmit = function(line) {
 };
 
 /**
- * When user presses the `up` key
- */
-
-Checkbox.prototype.onUpKey = function() {
-  var len = this.choices.realLength;
-  this.pointer = (this.pointer > 0) ? this.pointer - 1 : len - 1;
-  this.render();
-};
-
-/**
- * When user presses the `down` key
- */
-
-Checkbox.prototype.onDownKey = function() {
-  var len = this.choices.realLength;
-  this.pointer = (this.pointer < len - 1) ? this.pointer + 1 : 0;
-  this.render();
-};
-
-/**
- * When user presses a number key
- */
-
-Checkbox.prototype.onNumberKey = function(event) {
-  var num = Number(event.value);
-  if (num <= this.choices.realLength) {
-    this.pointer = num - 1;
-    this.question.choices.toggleChoice(this.pointer);
-  }
-  this.render();
-};
-
-/**
- * When user presses the `space` bar
- */
-
-Checkbox.prototype.onSpaceKey = function() {
-  this.spaceKeyPressed = true;
-  this.question.choices.toggleChoice(this.pointer);
-  this.render();
-};
-
-/**
  * Set the default value to use
  */
 
 Checkbox.prototype.setDefault = function() {
   if (this.question.hasDefault) {
-    var idx = this.question.choices.getIndex(this.question.default);
-    this.question.choices.enable(idx);
+    this.choices.enable(this.question.default);
   }
 };
 
@@ -158,99 +104,20 @@ Checkbox.prototype.setDefault = function() {
  * Get the currently selected value
  */
 
-Checkbox.prototype.getAnswerValue = function() {
-  this.selection = [];
-  var choices = this.question.choices.realChoices;
+Checkbox.prototype.getChecked = function() {
+  var choices = this.choices.items;
   var len = choices.length;
   var idx = -1;
   var res = [];
-
   while (++idx < len) {
     var choice = choices[idx];
     if (choice.checked && !choice.disabled) {
-      this.selection.push(choice.alias || choice.short);
+      this.checked.push(choice.short);
       res.push(choice.value);
     }
   }
   return res;
 };
-
-/**
- * Function for rendering checkbox choices
- * @param  {Number} pointer Position of the pointer
- * @return {String} Rendered choices string
- */
-
-function renderChoices(choices, pointer, options) {
-  var offset = 0;
-  var output = '';
-
-  choices.forEach(function(choice, i) {
-    if (choice.type === 'separator') {
-      offset++;
-      output += inactive(choice);
-
-    } else if (choice.disabled) {
-      offset++;
-      output += disabled(choice);
-
-    } else if (i - offset === pointer) {
-      output += active(choice, renderChoice(choice), options);
-
-    } else {
-      output += inactive(choice, renderChoice(choice));
-    }
-    output += ' \n';
-  });
-
-  return output.replace(/\n$/, '');
-}
-
-/**
- * Render the checkbox based on state.
- * @param  {Boolean} `checked` If enabled, adds an X to the checkbox
- * @return {String} returns the rendered radio-button string
- */
-
-function renderChoice(choice) {
-  return choice.checked ? log.green(radio.on) : radio.off;
-}
-
-/**
- * Utils
- */
-
-function disabled(choice) {
-  var symbol = process.platform === 'win32' ? ' (×) ' : ' ⓧ ';
-  return log.dim(symbol + choice.name + ' (' + (choice.disabled || 'Disabled') + ')');
-}
-
-function inactive(choice, prefix) {
-  return ' ' + (prefix || '') + ' ' + choice.name;
-}
-
-function active(choice, prefix, options) {
-  if (prefix && typeof prefix === 'object') {
-    options = prefix;
-    prefix = null;
-  }
-  return log.cyan(pointer(options)) + (prefix || '') + ' ' + choice.name;
-}
-
-function pointer(options) {
-  if (options && typeof options.pointer === 'string') {
-    return options.pointer.trim() + ' ';
-  }
-  switch(process.platform) {
-    case 'win32':
-      return '>';
-    case 'linux':
-      return '‣';
-    default: {
-      return '❯';
-    }
-  }
-}
 
 /**
  * Module exports
