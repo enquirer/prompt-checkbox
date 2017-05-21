af92d84988f93173f9b86960b6a74db1da9242f1
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-21 19:40:20 -0400

    refactor based on changes in prompt-base

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var debug = require('debug')('prompt-checkbox');
 var Prompt = require('prompt-base');
 var cyan = require('ansi-cyan');
-var red = require('ansi-red');
 
 /**
  * Checkbox prompt
@@ -12,12 +11,10 @@ var red = require('ansi-red');
 function Checkbox() {
   debug('initializing from <%s>', __filename);
   Prompt.apply(this, arguments);
-  this.firstRender = true;
-  this.helpMessage = this.options.helpMessage || '(Press '
+  this.errorMessage = null;
+  this.infoMessage = this.options.infoMessage || '(Press '
     + cyan('<space>')
     + ' to toggle)';
-
-  this.position = 0;
 }
 
 /**
@@ -27,30 +24,22 @@ function Checkbox() {
 Prompt.extend(Checkbox);
 
 /**
- * Render the prompt to the terminal
+ * Render all prompt choices to the terminal
  */
 
-Checkbox.prototype.render = function(state) {
-  var append = typeof state === 'string' ? red('>> ') + state : '';
+Checkbox.prototype.renderOutput = function() {
+  return this.choices.render(this.position, this.options);
+};
 
-  // render question
-  var message = this.message;
-  if (this.firstRender) {
-    this.firstRender = false;
-    message += this.helpMessage;
-  }
+/**
+ * Render only the selected choices to the terminal
+ */
 
-  // Render choices or answer depending on the state
-  if (this.status === 'answered') {
-    var keys = this.choices.checked.map(function(choice) {
-      return typeof choice === 'string' ? choice : choice.value;
-    });
-    message += cyan(keys.join(', '));
-  } else {
-    message += this.choices.render(this.position, this.options);
-  }
-
-  this.ui.render(message, append);
+Checkbox.prototype.renderAnswer = function() {
+  var keys = this.choices.checked.map(function(choice) {
+    return typeof choice === 'string' ? choice : choice.value;
+  });
+  return cyan(keys.join(', '));
 };
 
 /**
