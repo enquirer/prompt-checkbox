908009386e7dab0a767bbf74c71422e1bfcbc5cd
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-24 07:47:36 -0400

    adds long-list example

---
+++ examples/prompt.long-list.js
@@ -0,0 +1,31 @@
+var Question = require('prompt-question');
+var Prompt = require('..');
+var question = new Question('words', {
+  type: 'checkbox',
+  message: 'What choices do you want?',
+  choices: [
+    'foo',
+    'bar',
+    'baz',
+    'qux',
+    'fez',
+    'aaa',
+    'bbb',
+    'ccc',
+    'ddd',
+    'eee',
+    'fff',
+    'ggg',
+    'hhh',
+    'iii'
+  ]
+});
+
+var prompt = new Prompt(question);
+prompt.run()
+  .then(function(answers) {
+    console.log(arguments)
+  })
+  .catch(function(err) {
+    console.log(err)
+  })
